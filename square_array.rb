3b09ab9900e10581eae45dbef94fda4955746dfc
Dani Bassett <dani@danithetallgirl.com>
2020-07-17 01:50:43 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
   numbers = [1,2,3]
   new_array = []
-  array.each {|numbers| new_array << numbers ** 2}
+  array.each {|numbers| new_array << numbers * 2}
   new_array
 end
